d405848d52c4ade0e76df519c521045718abdba5
hanmoongoo <hanmoongoo@gmail.com>
2021-04-21 20:38:27 +0900

    create tweeting form and add with firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,37 @@
-import React from 'react'
+import { dbService } from 'fbase'
+import React, { useState } from 'react'
 
-const Home = () => <span>Home</span>
+const Home = () => {
+    const [nweet, setNweet] = useState("")
+    const onSubmit = async(event) => {
+        event.preventDefault()
+        await dbService.collection("nweets").add({
+            nweet,
+            createAt: Date.now(),            
+        })
+        setNweet("")
+    }
+    const onChange = (event) => {
+        const {
+            target:{ value }
+        } = event
+        setNweet(value)
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    type="text"
+                    placeholder="what's on mind?" 
+                    maxLength={120} 
+                    value={nweet} 
+                    onChange={onChange} 
+                />
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    )
+}
+
 export default Home
